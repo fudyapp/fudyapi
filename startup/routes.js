323ef4e08e50ba6a37b893536cb07f71c3e0cb32
prem@capiot.com <prem@capiot.com>
2019-03-28 22:15:45 +0530

    changes

--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,4 @@
 const express = require('express');
-const genres = require('../routes/genres');
-const customers = require('../routes/customers');
-const movies = require('../routes/movies');
-const rentals = require('../routes/rentals');
-const returns = require('../routes/returns');
 const users = require('../routes/users');
 const admin = require('../routes/adminUsers');
 const auth = require('../routes/auth');
@@ -19,13 +14,8 @@ const orders = require('../routes/orders');
 
 const error = require('../middleware/error');
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(express.json());
-  // app.use('/api/genres', genres);
-  // app.use('/api/customers', customers);
-  // app.use('/api/movies', movies);
-  // app.use('/api/rentals', rentals);
- // app.use('/api/returns', returns);
   app.use('/api/users', users);
   app.use('/api/auth', auth);
   app.use('/api/admin', admin);
@@ -37,8 +27,6 @@ module.exports = function(app) {
   app.use('/api/tags', tags);
   app.use('/api/wallet', wallet);
   app.use('/api/preference', userPreference);
-   app.use('/api/orders', orders);
-
-
+  app.use('/api/orders', orders);
   app.use(error);
 }
